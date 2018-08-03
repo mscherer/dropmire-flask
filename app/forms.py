a58d89dc19fa023eca452e798a13e44f42ca4b83
Michael Scherer <misc@redhat.com>
2018-08-03 11:38:39 +0200

    Add a domain verification routine
    We want to filter email by making sure that's only from a specific organisation

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import Domain
+from app import application
 
 # TODO add a better validator
 #   check kerberos ID (or email if a alias ?)
@@ -10,6 +11,13 @@ class AddDomainForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     domain = StringField('Domain', validators=[DataRequired()])
     submit = SubmitField('Add the domain')
+
+    def validate_email(self, email):
+        d = application.config.get('EMAIL_DOMAIN')
+        if d:
+            if not email.data.split('@')[-1].lower() == d.lower():
+                raise ValidationError("Invalid email domain entered, should be %s" % d)
+
 
     def validate_domain(self, domain):
         domain = Domain.query.filter_by(domain=domain.data.lower().strip()).first()
